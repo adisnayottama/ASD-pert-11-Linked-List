68734cc73d15c8e9afc0712fdf2f27f60b7dde94
adisnayottama <adisnayottama1@gmail.com>
2026-05-06 20:51:35 -1200

    Prc1 Pert11 Class2

---
+++ Pertemuan11/Node02.java
@@ -0,0 +1,13 @@
+package Pertemuan11;
+
+import org.w3c.dom.Node;
+
+public class Node02 {
+    Mahasiswa02 data;
+    Node02 next;
+
+    public Node02(Mahasiswa02 data, Node02 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
